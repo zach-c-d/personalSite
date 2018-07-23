6a0a449adad8ca950816b1979425c4fae8c83238
Zachary Donsky <zachary.donsky@gmail.com>
2018-07-23 18:08:11 -0400

    added new assets

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    console.log('up!');
+    var currentProjects = document.getElementById("CurrentProjects");
+
+    currentProjects.addEventListener('click', 
+    function(){
+        console.log('hello!');
+    }); 
+}
+
+// let duh_body = document.querySelector('body');
+
+
