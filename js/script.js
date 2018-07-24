768b13efdaa48accacf26923ba0960426eef8975
Zachary Donsky <zachary.donsky@gmail.com>
2018-07-24 18:58:01 -0400

    added another asset (chalmersBatch)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 window.onload = function(){
     console.log('up!');
     var currentProjects = document.getElementById("CurrentProjects");
+    var resumePortfolio = document.getElementById("ResumePortfolio");
+    
 
     currentProjects.addEventListener('click', 
+    function(){
+        console.log('hello!');
+        currentProjects.classList.add("boxExpanded");
+    }); 
+    resumePortfolio.addEventListener('click', 
     function(){
         console.log('hello!');
     }); 
